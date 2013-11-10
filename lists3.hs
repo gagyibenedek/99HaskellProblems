9e573136d3bb9636dd3a0976b64e170983d17854
Benedek Gagyi <gagyibenedek@yahoo.com>
2013-11-10 21:27:31 +0100

    P22 added

--- lists3.hs
+++ lists3.hs
@@ -4,3 +4,9 @@ p21 y (x:xs) i
     | i < 2 = y : (x:xs)
 	| i > length (x:xs) = (x:xs) ++ [y]
 	| otherwise = x : (p21 y xs (i - 1))
+	
+p22 :: Int -> Int -> [Int]
+p22 a b 
+    | a == b = [a]
+	| a > b = p22 b a
+	| otherwise = a : p22 (a+1) b
